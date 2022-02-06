c758ac305b7fa910ceee00302f5fe1e01683c9ac
pratiked7 <pratiked7@gmail.com>
2022-02-06 12:02:45 +0530

    Dai contract

---
+++ contracts/Dai.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.2;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract Dai is ERC20 {
+
+    constructor() ERC20("Dai Stablecoin", "DAI") {
+
+    }
+
+    function faucet(address to, uint amount) external {
+        _mint(to, amount);
+    }
+}
